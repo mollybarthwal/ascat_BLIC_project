f0e5f1aac803fc25528ad5eb88ee82687165d11c
mollybarthwal <126357038+mollybarthwal@users.noreply.github.com>
2024-05-22 22:17:19 +0100

    Update ASCAT_E28_E56.R

--- ASCAT_E28_E56.R
+++ ASCAT_E28_E56.R
@@ -2,7 +2,6 @@ library(ASCAT)
 library(dplyr)
 setwd("~/Downloads/ascat_intro/")
 
-
 ### 1. Running ASCAT with E28 only
 ascat.bc = ascat.loadData(Tumor_LogR_file = "E28_chr10_deletion/E28_CL_new.LogR",
                           Tumor_BAF_file = "E28_chr10_deletion//E28_CL_new.BAF", 
@@ -14,7 +13,6 @@ ascat.bc = ascat.aspcf(ascat.bc)
 ascat.plotSegmentedData(ascat.bc, img.prefix = "Non_mixed_28")# penalty=25 for targeted sequencing data
 ascat.output = ascat.runAscat(ascat.bc, gamma = 1) # gamma=1 for HTS data
 
-
 ### 2. Running ASCAT with E56 only
 ascat.bc = ascat.loadData(Tumor_LogR_file = "E56_chr10_neutral/E56_CL_new.LogR",
                           Tumor_BAF_file = "E56_chr10_neutral/E56_CL_new.BAF", 
@@ -25,8 +23,6 @@ ascat.plotRawData(ascat.bc, img.prefix = "Non_mixed_56_checking")
 ascat.bc = ascat.aspcf(ascat.bc)
 ascat.plotSegmentedData(ascat.bc, img.prefix = "Non_mixed_56")# penalty=25 for targeted sequencing data
 ascat.output = ascat.runAscat(ascat.bc, gamma = 1) # gamma=1 for HTS data
-
-
 
 ### 3. Mixing data -- currently we are trying to add portions of chr10 from E28_CL into E56_CL
 ### Loading data 
@@ -60,13 +56,10 @@ gl_mix.BAF[rows, ] <- gl_d10.BAF[rows, ]
 cl_mix.logR[rows, ] <- cl_d10.logR[rows, ]
 cl_mix.BAF[rows, ] <- cl_d10.BAF[rows, ]
 
-
-
 gl_mix.logR %>%  write.table(.,"gl_mix.logR", sep = "\t", quote = F, row.names = F)
 gl_mix.BAF %>% write.table(., "gl_mix.BAF", sep = "\t", quote = F, row.names = F)
 cl_mix.logR %>% write.table(., "cl_mix.logR", sep = "\t", quote = F, row.names = F)
 cl_mix.BAF %>% write.table(., "cl_mix.BAF", sep = "\t", quote = F,  row.names = F)
-
 
 ascat.bc = ascat.loadData(Tumor_LogR_file = "cl_mix.logR", 
                           Tumor_BAF_file = "cl_mix.BAF", 
@@ -78,7 +71,5 @@ ascat.plotRawData(ascat.bc, img.prefix = "Mixed_twofifthE28_inE56")
 ascat.bc = ascat.aspcf(ascat.bc)
 ascat.plotSegmentedData(ascat.bc, img.prefix = "Mixed_twofifthE28_inE56")# penalty=25 for targeted sequencing data
 ascat.output = ascat.runAscat(ascat.bc, gamma = 1) 
-
-
 
 sessionInfo()
